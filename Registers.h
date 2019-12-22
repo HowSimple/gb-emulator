2dd1f3cd363e4a04626450dd160b33e799b497dd
HowSimple <43933950+HowSimple@users.noreply.github.com>
2019-12-21 21:58:37 -0600

    Implemented more instructions

--- Registers.h
+++ Registers.h
@@ -1,51 +1,132 @@
 #pragma once
 #include <cstdint>
+#include <bitset>
+#include <iostream>
+#include <cstdlib>
 
 
-typedef uint8_t _8b;
-typedef uint16_t _16b;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef unsigned char u1;
+enum flag { ZERO = 7, SUBT = 6, HALFC = 5, CARRY = 4};
+enum op_type {ADD, ADC,SUB, LD, NOP, OR, AND, CP};
 
-struct Registers
-{	// registers can be accessed individually or as a pair
+
+class Registers
+{
+	public:
+		Registers();
+
+		void op_dec(u16& target);
+
+		void op_or(u8& target, u8 source);
+
+		void op_dec(u8& target);
+
+		void op_ld(u8& target, u8 const source);
+
+		void op_ld(u16& target, u16 source);
+
+		void op_and(u8& target, u8 source);
+
+		void op_xor(u8& target, u8 source);
+
+		void op_swap(u8& target);
+
+		void op_swap(u16& target);
+
+		u1 get_carry();
+
+		u1 get_halfc();
+
+		u1 get_zero();
+
+		u1 get_sub();
+
+		void set_carry(bool x);
+
+		void set_halfc(bool x);
+
+		void set_zero(bool x);
+
+		void set_sub(bool x);
+
+		void op_nop();
+
+		void update_halfc(u8 target, u8 source);
+		void update_halfc(u16 target, u16 source);
+		void update_carry(u8 target, u8 source);
+		void update_carry(u16 target, u16 source);
+
+		void update_zero(u16 target);
+
+		void update_zero(u8 target);
+
+		void update_flags(bool zero, bool subt, bool carry, bool halfc);
+
+		// arithmetic instructions
+		void op_add(u8& target, u8 source);
+
+		void op_sub(u8& target, u8 source);
+
+		void op_adc(u8& target, u8 source);
+
+	
+
+		void op_cp(u16& target, u16 const value);
+		void op_cpl(u8& target);
+		  // increment/decrement instructions
+
+		void op_inc(u8& target);
+		struct flags {
+			bool zero; 
+			bool subt;
+			bool halfc;
+			bool carry;
+
+		};
+			   
+	
+	// registers can be accessed individually or as a pair
 	// each pair of registers occupies the same 16bit location
+	
+		u8 a;
+		flags f;
+				
+			
+			
+	
+	
 	struct {
 		union {
 			struct {
-				_8b f;
-				_8b a;
+				u8 c;
+				u8 b;
 			};
-			_16b af;
+			u16 bc;
 		};
 	};
 	struct {
 		union {
 			struct {
-				_8b c;
-				_8b b;
+				u8 e;
+				u8 d;
 			};
-			_16b bc;
+			u16 de;
 		};
 	};
 	struct {
 		union {
 			struct {
-				_8b e;
-				_8b d;
+				u8 l;
+				u8 h;
 			};
-			_16b de;
+			u16 hl;
 		};
 	};
-	struct {
-		union {
-			struct {
-				_8b l;
-				_8b h;
-			};
-			_16b hl;
-		};
-	};
-	_16b sp;
-	_16b pc;
+	u16 sp;
+	u16 pc;
+
 
 };
 
