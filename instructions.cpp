084bed75f06afd35fd5f9616cb2fef0fd6dd6680
HowSimple <43933950+HowSimple@users.noreply.github.com>
2019-12-27 18:31:37 -0600

    Added appropriate flag behavior for several op functions. Added TODO tags on unimplemented functions to easily find them

--- instructions.cpp
+++ instructions.cpp
@@ -54,18 +54,24 @@ void CPU::op_ld(u16& target, u16 source) // load source to target
 
 void CPU::op_inc(u8& target) // increment target
 {
+	u8 temp = target;
 	target++;
-	update_zero(target);
+	f.zero = 0;
+	f.subt = 0;
+	update_halfc(target, temp);
 }
 
 void CPU::op_inc(u16& target) // increment target
 {
 	target++;
-	update_zero(target);
 }
 void CPU::op_dec(u8& target) // decrement target 
 {
-	target -= 1; 	
+	update_halfc(target, --target);
+	//target--;
+	update_zero(target);
+	f.subt = 0;
+	
 }
 
 void CPU::op_dec(u16& target) // decrement target 
@@ -77,15 +83,23 @@ void CPU::op_dec(u16& target) // decrement target
 
 
 
-void CPU::op_swap(u8& target) // rotate upper/lower 4bits in target
+void CPU::op_swap(u8& target) // swap upper/lower 4bits in target
 {
 	target = target << 4;
+	f.halfc = 0;
+	f.subt = 0;
+	update_zero(target);
 }
-void CPU::op_swap(u16& target) // rotate upper/lower 8bits in target
+void CPU::op_swap(u16& target) // swap upper/lower 8bits in target
 {
 	target = target << 8;
+	f.halfc = 0;
+	f.subt = 0;
+	update_zero(target);
+}
+
 // rotate instructions
-}void CPU::op_rra()
+void CPU::op_rra()
 {
 	bool temp = f.carry;
 	f.carry = (a & (1 << 7)) >> 7;
@@ -98,14 +112,19 @@ void CPU::op_rrc(u8& reg) // right-rotate reg A and carry flag by 1 bit
 	bool temp = f.carry;
 	f.carry = (a & (1 << 7)) >> 7;
 	a = ( (a << 1) | f.carry << 7) ;
-	update_zero(a);
+	f.zero = 0;
+	f.halfc = 0;
+	//TODO: update_carry()
+
 }
 void CPU::op_rrca() // right-rotate reg A and carry flag by 1 bit
 {
 	bool temp = f.carry;
 	f.carry = (a & (1 << 7)) >> 7;
 	a = ((a << 1) | f.carry << 7);
-	update_zero(a);
+	f.halfc = 0;
+	f.subt = 0;
+	f.zero = 0;
 }
 
 void CPU::op_rr(u8& value)	// rotate right. 
@@ -114,6 +133,8 @@ void CPU::op_rr(u8& value)	// rotate right.
 	f.carry = (a & (1 << 7)) >> 7;
 
 	a = ((a << 1) | temp << 7);
+	f.halfc = 0;
+	f.subt = 0;
 	update_zero(a);
 }
 void CPU::op_rlca() // left-rotate reg A and carry flag by 1 bit
@@ -125,33 +146,42 @@ void CPU::op_rlca() // left-rotate reg A and carry flag by 1 bit
 	update_zero(a);
 }
 
-void CPU::op_rlc(u8& value)	// rotate left. bit 7 to bit 0. bit 7 to carry.
+void CPU::op_rlc(u8& reg)	// rotate left. bit 7 to bit 0. bit 7 to carry.
 {
 	
-	f.carry = (a & (1 << 7)) >> 7;
-	a = ((a << 1) | f.carry << 7);
-	update_zero(a);
+	f.carry = (reg & (1 << 7)) >> 7;
+	reg = ((reg << 1) | f.carry << 7);
+	update_zero(reg);
+	f.halfc = 0;
+	f.subt = 0;
 }
-void CPU::op_rl(u8& value)	// rotate left. carry to bit 0. bit 7 to carry.
+void CPU::op_rl(u8& reg)	// rotate left. carry to bit 0. bit 7 to carry.
 {
 	bool temp = f.carry;
 	f.carry = (a & (1 << 7)) >> 7;
 
 	a = ((a << 1) | temp << 7);
 	update_zero(a);
+	f.halfc = 0;
+	f.subt = 0;
 }
 // shift instructions
 void CPU::op_sla(u8& value)	// shift left. bit 7 to carry flag. bit 0 set to 0.
-{
-
+{// TODO: needs implementation
+	f.halfc = 0;
+	f.subt = 0;
 }
 void CPU::op_sra(u8& value)	// shift right. bit 0 to carry flag. 
-{
-
+{// TODO: needs implementation
+	f.carry = 0;
+	f.halfc = 0;
+	f.subt = 0;
 }
 void CPU::op_srl(u8& value)	// shift right. bit 0 to carry flag. bit 7 set to 0. 
-{
-
+{// TODO: needs implementation
+	f.carry = 0;
+	f.halfc = 0;
+	f.subt = 0;
 }
 
 
@@ -181,7 +211,7 @@ void CPU::op_sub(u8& target, u8 source) // adds value at source to target
 	update_zero(target);
 }
 void CPU::op_sbc(u8& target, u8 source) // subtract with carry
-{
+{// TODO: needs implementation
 
 }	
 void CPU::op_adc(u8& target, u8 source) // adds value at source and carry to target. 
@@ -208,7 +238,9 @@ void CPU::op_xor(u16 target) // bitwise XOR on A and target, store in A
 {
 	a = a ^ target;
 	update_zero(a);
-	
+	f.subt = 0;
+	f.halfc = 0;
+	f.carry = 0;
 }
 void CPU::op_or(u8 target) // bitwise OR on A and target, store in A 
 {
@@ -220,36 +252,43 @@ void CPU::op_or(u16 target) // bitwise OR on A and target, store in A
 {
 	a = a | target;
 	update_zero(a);
-	
+	f.subt = 0;
+	f.halfc = 0;
+	f.carry = 0;
 }
 void CPU::op_and(u8 target) // bitwise AND on A and target, store in A 
 {
 	a = a & target;
 	update_zero(a);
-	set_halfc(1);
-	
+	f.subt = 0;
+	f.halfc = 1;
+	f.carry = 0;
 }
 void CPU::op_and(u16 target) // bitwise AND on A and target, store in A 
 {
 	a = a & target;
 	update_zero(a);
-	set_halfc(1);
-	
+	f.subt = 0;
+	f.halfc = 1;
+	f.carry = 0;	
 }
 // flag instructions 
 void CPU::op_ccf() // compliment carry flag
 {
 	f.carry = ~f.carry;
-
+	f.halfc = 0;
+	f.subt = 0;
 }
 void CPU::op_scf() // set carry flag to true
 {
 	f.carry = true;
+	f.halfc = 0;
+	f.subt = 0;
 }
 
 // jump instructions
 void CPU::op_jp() 
-{
+{// TODO: needs implementation
 	
 }
 void CPU::op_jp(bool condition) // push reg to stack
@@ -259,7 +298,7 @@ void CPU::op_jp(bool condition) // push reg to stack
 	else cycles -= 4;
 }
 void CPU::op_jr()
-{
+{// TODO: needs implementation
 
 }
 void CPU::op_jr(bool condition)
@@ -285,7 +324,7 @@ void CPU::op_push(u16 value) // push value to stack
 // call instructions
 void CPU::op_rst(u8 address)	// 'restart'. push current address to stack, jump to address
 {
-
+	op_call(address);
 }
 
 void CPU::op_call(bool condition) // conditionally call address
@@ -295,7 +334,7 @@ void CPU::op_call(bool condition) // conditionally call address
 	else cycles -= 4;
 }
 void CPU::op_call() // call address
-{
+{	// TODO: needs implementation
 
 
 
@@ -308,26 +347,26 @@ void CPU::op_ret(bool condition) // conditionally return
 	else cycles -= 4;
 }
 void CPU::op_ret()
-{
+{// TODO: needs implementation
 
 }
 // bit instructions
 
 void CPU::op_bit(u1 bit, u8& reg) // test bit 
 {
-
+	//u1 bit = (reg >> bit) & 1;
+	update_zero(reg);
+	f.subt = 0;
+	f.halfc = 0;
 }
 void CPU::op_set(u1 bit, u8& reg) // set bit
 {
-
+	reg |= 1 << bit;
 }
 void CPU::op_res(u1 bit, u8& reg) // reset bit
 {
-
+	reg &= ~(1 << bit);
 }
-
-
-
 
 
 
@@ -335,19 +374,19 @@ void CPU::op_res(u1 bit, u8& reg) // reset bit
 
 
 void CPU::op_nop() // 'no-op'
-{
+{// TODO: needs implementation
 	
 }
 void CPU::op_undefined() // undefined instruction
-{
+{// TODO: needs implementation
 
 }
 void CPU::op_halt() // stop CPU until interrupt
-{
+{// TODO: needs implementation
 
 }
 void CPU::op_stop() // stop CPU and video, wait until next button press
-{
+{// TODO: needs implementation
 
 }
 
@@ -357,22 +396,22 @@ void CPU::op_cpl(u8 target) //  set target to complement of target
 
 }
 void CPU::op_cp(u16 target)
-{
+{// TODO: needs implementation
 
 
 }
 
 // interrupt instructions
 void CPU::op_di() // disable interrupts after next instruction
-{
+{// TODO: needs implementation
 
 }
 void CPU::op_ei() // enable interrupts after next instruction
-{
+{// TODO: needs implementation
 
 }
 
 void CPU::op_daa() // enable interrupts after next instruction
-{
-
+{// TODO: needs implementation
+	f.halfc = 0;
 }
