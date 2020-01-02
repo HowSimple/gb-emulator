13d8ab1818981bd55e3096a509fee8ee0817fab0
HowSimple <43933950+HowSimple@users.noreply.github.com>
2020-01-01 18:59:37 -0600

    Moved screen to GPU class

--- Gameboy.h
+++ Gameboy.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "CPU.h"
-#include <iostream>
 #include <cstdint>
 #include<vector>
 #include<string>
@@ -9,19 +8,28 @@
  
 typedef uint8_t u8;
 typedef uint16_t u16;
+enum interrupt { vblank = 0, lcdstat = 1, timer = 2, serial = 3, joypad = 4 };
 
 
-class Gameboy : CPU
+class Gameboy : public CPU
 {
 public:
 	Gameboy(std::string filename);
 	Gameboy();
-	u8 screen[160][144][3];
-	std::vector<u8> cart;
-	int cart_size;
-	void load_cartridge(std::string filename);
-		
-
-		
+	 std::vector<u8> cart;
 	
+	int cart_size;
+	enum class rom_bank { MBC1, MBC2, MBC3, MBC4, MBC5, OFF } mode;
+	bool has_cart_battery;
+	bool has_cart_ram;
+	
+	void load_cartridge(std::string filename);
+
+	void load_ram();
+	void load_boot_rom();
+	void set_cart_type();
+
+	
+	void request_interrupt(interrupt x);
+
 };
